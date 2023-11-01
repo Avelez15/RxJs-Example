71ddd6c9cabec78ccedecd432e77deae97666fe3
Alejandro Velez <velez.alejandro4@gmail.com>
2023-11-01 19:47:44 -0400

    added increment and decrement reducer and action implementation

--- src/app/counter-store/counter.reducer.ts
+++ src/app/counter-store/counter.reducer.ts
@@ -1,5 +1,10 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.actions';
 
 const initalState = 0;
 
-export const counterReducer = createReducer(initalState);
+export const counterReducer = createReducer(
+  initalState,
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state) => state - 2)
+);
